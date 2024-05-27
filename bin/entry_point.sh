a40af2cafed9378c16f0c97c8097371cf9e6dd73
tlannaax <tlannaax@outlook.com>
2024-05-27 14:21:41 +0000

    Fix entry_point.sh

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -2,19 +2,20 @@
 
 CONFIG_FILE=_config.yml 
 
+jekyll_pid=$(pgrep -f jekyll)
+kill -KILL $jekyll_pid
+
 /bin/bash -c "exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
 SY=$(uname -r)
 
 if [[ $SY = *"WSL"* ]]; then
-
-while true; do
-
-    SUM1=$(md5sum "$CONFIG_FILE")
+  SUM=$(md5sum "$CONFIG_FILE")
+  while true; do
     sleep 2
-    SUM2=$(md5sum "$CONFIG_FILE")
-    if [ "$SUM1" != "$SUM2" ];
-    then
+    SUMN=$(md5sum "$CONFIG_FILE")
+    if [ "$SUM" != "$SUMN" ]; then
+    SUM=$SUMN
     echo "Change detected to $CONFIG_FILE, restarting Jekyll"
 
     jekyll_pid=$(pgrep -f jekyll)
@@ -22,9 +23,7 @@ while true; do
 
     /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
     fi
-
-done
-
+  done
 else
 
 while true; do
